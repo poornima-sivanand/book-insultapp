16c9a16cfd833dc3d59705b358a8f4d339f404ae
poornima-sivanand <poornima.sivanand@gov.bc.ca>
2019-01-28 12:53:10 -0800

    alter app

--- src/main/java/org/openshift/InsultResource.java
+++ src/main/java/org/openshift/InsultResource.java
@@ -12,5 +12,9 @@ public class InsultResource {
 
     @GET()
     @Produces("application/json")
-    public HashMap<String,String> getInsult() 
+    public HashMap<String,String> getInsult() {
+        HashMap<String,String> theInsult = new HashMap<String,String>();
+        theInsult.put("insult", new InsultGenerator().generateInsult());
+        return theInsult;
+    }
 }
